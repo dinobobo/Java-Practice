1b9910361ea2981bfd95bc5c3eabffa860c701e5
Yanping Cai <40920652+dinobobo@users.noreply.github.com>
2019-09-06 08:41:46 -0400

    Add new test codes

---
+++ HelloWorld/src/MethodCall.java
@@ -0,0 +1,11 @@
+public class MethodCall {
+    public static void main(String[] args) {
+        //Call the toPrint() method here
+        toPrint();
+    }
+
+    //Create a toPrint() here and in the method body write a print statement to print 'Print Method'
+    public static void toPrint(){
+        System.out.println("Print Method");
+    }
+}
