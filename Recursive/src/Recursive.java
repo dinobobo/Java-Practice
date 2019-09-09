bdaff42fada57ada10728cfaca58e32e77b2345d
dinobobo <kenzocyp@gmail.com>
2019-09-09 17:09:49 -0400

    more testing

---
+++ Recursive/src/Recursive.java
@@ -0,0 +1,17 @@
+public class Recursive {
+    public static void main(String[] args){
+        int num = 10;
+        int ans = factorial(num);
+        System.out.println("The factorial of " + num + " is " + ans);
+    }
+    public static int factorial(int i){
+        if(i == 1){return 1;}
+            else{return factorial(i-1)*i;}
+    }
+
+    public static void fibonacci(int upper){
+        for(i = 0; i <= upper; i++){
+
+        }
+    }
+}
