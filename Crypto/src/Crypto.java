df9a4cbe6730140621196178fab7ac21845dfffb
Yanping Cai <40920652+dinobobo@users.noreply.github.com>
2019-09-10 07:16:18 -0400

    Simple encrypto finished

--- Crypto/src/Crypto.java
+++ Crypto/src/Crypto.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 public class Crypto {
     public static void main(String[] args){
-        //Scanner input = new Scanner(System.in);
-        //System.out.println("What is the text you want to normalize?");
-        //String text = input.nextLine();
-        //String NormalizedText = normalizeText(text);
-        //System.out.println("The normalized text is " + NormalizedText);
-        //String encryptedText = caesarify(NormalizedText, 10);
-        //System.out.println("The Encrypted Text is " + encryptedText );
-        System.out.println(groupify("adfagwesadg", 3));
+        Scanner input = new Scanner(System.in);
+        System.out.println("What string you want to encrypt?");
+        String s = input.nextLine();
+        System.out.println("How much you want to shift and what is the group size?");
+        String size = input.nextLine();
+        Scanner sizeScanner = new Scanner(size);
+        int shift = sizeScanner.nextInt();
+        int groupSize = sizeScanner.nextInt();
+        String result = encryptString(s, shift, groupSize);
+        System.out.println("Your encrypted string is " + result);
+        String DecryptResult = decryptString(result,shift);
+        System.out.println("Your decrypted result is " + DecryptResult);
+
+
     }
     public static String normalizeText(String text){
         String TextNoSpace = text.replaceAll("\\s", "");
@@ -53,5 +59,24 @@ public class Crypto {
         ans += s.substring(i - num);
         ans += "x".repeat(i - s.length());
         return ans;
+    }
+    public static String encryptString(String s, int shift, int groupSize){
+        String NormalizedText = normalizeText(s);
+        String encryptedText = caesarify(NormalizedText, shift);
+        return groupify(encryptedText, groupSize);
+    }
+    public static String ungroupify(String s){
+        String sNoSpace =  s.replaceAll("\\s","");
+        int xIndex = sNoSpace.indexOf('x');
+        if(xIndex == -1){
+            return sNoSpace;
+        }else{
+            return sNoSpace.substring(0,xIndex);
+        }
+
+    }
+    public static String decryptString(String s, int shift){
+        String normalizedS = ungroupify(s);
+        return caesarify(normalizedS, -shift);
     }
 }
